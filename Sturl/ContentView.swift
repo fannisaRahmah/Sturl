c0535aa8037db8e888b3e8f5f1fe8552f2a4e40f
fannisaRahmah <fannisa.azwar2@gmail.com>
2022-07-27 13:36:49 +0700

    Initial Commit

---
+++ Sturl/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Sturl
+//
+//  Created by Fannisa Rahmah on 27/07/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Sturl/SturlApp.swift
@@ -0,0 +1,17 @@
+//
+//  SturlApp.swift
+//  Sturl
+//
+//  Created by Fannisa Rahmah on 27/07/22.
+//
+
+import SwiftUI
+
+@main
+struct SturlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
